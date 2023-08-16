00f185ebe1a7c5f8818110c7aa741c042468b5a8
ShuheiKurinami <zhoupinglibo252@gmail.com>
2023-08-16 17:41:30 +0900

    今日の一応やったこと

---
+++ src/010_useState_to_useReducer/start/Example.js
@@ -0,0 +1,39 @@
+import { useReducer, useState } from "react";
+
+const Example = () => {
+  const [state, setState] = useState(0);
+  const [rstate, dispatch] = useReducer((prev, action) => {
+    if (action === "+") {
+      return ++prev;
+    } else if (action === "-") {
+      return --prev;
+    }
+  }, 0);
+
+  const countUp = () => {
+    setState((prev) => ++prev);
+  };
+  const rcountUp = () => {
+    dispatch("+");
+  };
+
+  const rcountDown = () => {
+    dispatch("-");
+  };
+
+  return (
+    <>
+      <div>
+        <h3>{state}</h3>
+        <button onClick={countUp}>+</button>
+      </div>
+      <div>
+        <h3>{rstate}</h3>
+        <button onClick={rcountUp}>+</button>
+        <button onClick={rcountDown}>-</button>
+      </div>
+    </>
+  );
+};
+
+export default Example;
